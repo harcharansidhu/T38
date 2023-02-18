1623f8378c9178c07bf40570e817671b89c5cdcb
Harcharan Singh Sidhu <harcharan.singh.sidhu@gmail.com>
2023-02-18 14:29:57 +0000

    Added watched movie and setup tabular printout of summary of recommendation.

--- watch_next.py
+++ watch_next.py
@@ -2,7 +2,10 @@
 # Compulsory Task 2
 
 # IMPORTS
+# language model library
 import spacy
+# table making library
+from tabulate import tabulate
 
 # Specify the language model
 nlp = spacy.load('en_core_web_md')
@@ -46,3 +49,18 @@ def movie_recommender(description):
             movie_to_recommend = movie_title
     
     return movie_to_recommend
+
+# Set up variables to specify watched movie
+movie_watched = "Planet Hulk"
+movie_description = "Will he save their world or destroy it? When the Hulk becomes too dangerous for the Earth, the Illuminati trick Hulk into a shuttle and launch him into space to a planet where the Hulk can live in peace. Unfortunately, Hulk land on the planet Sakaar where he is sold into slavery and trained as a gladiator."
+
+# get a movie recommendation based on movie watched
+movie_recommendation = movie_recommender(movie_description)
+
+# Print summary
+summary = []
+summary.append(["Movie watched", movie_watched])
+summary.append(["Movie description", movie_description])
+summary.append(["Movie recommended", movie_recommendation])
+
+print(tabulate(summary, tablefmt="simple_grid", maxcolwidths=[None, 50]))
